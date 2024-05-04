95c0b282da5d3f3512836340f39070d7c859d3fc
kyzooghost <jeffersonzjtang@gmail.com>
2024-05-04 18:15:59 +1000

    fork tests passing

--- test/RampForkTest.sol
+++ test/RampForkTest.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.0;
 
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 import { TestBase } from './TestBase.sol';
 import { Ramp } from '../src/Ramp.sol';
 import { Verifier } from '../src/Verifier.sol';
@@ -16,6 +19,8 @@ contract RampForkTest is TestBase {
     IEscrowManager escrowManager;
     IOrderManager orderManager;
     IUserManager userManager;
+
+    address USDC = 0x036CbD53842c5426634e7929541eC2318f3dCF7e;
 
     function setUp() public {
         ConstructorArgs constructorArgs = new ConstructorArgs();
@@ -45,7 +50,7 @@ contract RampForkTest is TestBase {
     }
 
     function test_ManagerContractsDirectlyCalled_ShouldRevert() public {
-        vm.prank(DEPLOYER);
+        vm.startPrank(DEPLOYER);
         
         TokenAndFeed[] memory tokenAndFeeds = new TokenAndFeed[](1);
         tokenAndFeeds[0] = TokenAndFeed(address(1), address(1));
@@ -79,15 +84,45 @@ contract RampForkTest is TestBase {
 
         vm.expectRevert();
         escrowManager.uncommitDeposit(address(1), address(1), 1);
+
+        vm.stopPrank();
     }
 
     function test_AddValidTokens() public {
-        vm.prank(DEPLOYER);
         TokenAndFeed[] memory tokenAndFeeds = new TokenAndFeed[](1);
         tokenAndFeeds[0] = TokenAndFeed({
             token: address(1),
             feed: address(1)
         });
+        vm.prank(DEPLOYER);
         ramp.addValidTokens(tokenAndFeeds);
+    }
+
+    function test_AddOrder() public {
+        vm.prank(USER);
+        // Add order for USDC on Base testnet
+        ramp.addOrder(address(1), USDC, 1, 1e8, 1);
+    }
+
+    function test_deposit() public {
+        vm.startPrank(USER);
+        deal(USDC, USER, 1e18);
+        IERC20(USDC).approve(address(ramp), 1e18);
+        // Add order for USDC on Base testnet
+        ramp.deposit(USDC, 1);
+        vm.stopPrank();
+    }
+
+    function test_commitOrder() public {
+        vm.prank(USER);
+        // Add order for USDC on Base testnet
+        bytes32 orderId = ramp.addOrder(address(1), USDC, 1, 1e8, 1);
+
+        vm.startPrank(USER_2);
+        deal(USDC, USER_2, 1e18);
+        IERC20(USDC).approve(address(ramp), 1e18);
+        // Add order for USDC on Base testnet
+        ramp.deposit(USDC, 1);
+        ramp.commitOrder(orderId, 1e8);
     }
 }
