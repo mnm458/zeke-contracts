b96fa35a73b3efd58e81499ab666f4ded389b9b8
kyzooghost <jeffersonzjtang@gmail.com>
2024-05-04 23:53:46 +1000

    add comment to bash script

--- script/test.sh
+++ script/test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Get current environment variables defined in .env
+# Fork block number is for Base Testnet
+
 source .env
 echo "Running tests"
 forge test --fork-url $(grep ETH_RPC_URL .env | cut -d '=' -f2) --fork-block-number 9515700 -vvv --via-ir
